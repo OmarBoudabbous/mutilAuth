7d5a9c017333d149720f3ec3a902f6faf60eee96
Amor Boudabbous <omar.boudabbous@outlook.fr>
2024-12-16 19:43:28 +0100

    Adding Admin Dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,6 +8,6 @@ class AdminController extends Controller
 {
     public function adminDashboard(){
 
-        return view('role.admin.dashboard');
+        return view('role.admin.index');
     }
 }
--- resources/views/role/admin/dashboard.blade.php
+++ resources/views/role/admin/dashboard.blade.php
@@ -1,12 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>admin dashboard</title>
+    <meta name="description" content="Responsive HTML Admin Dashboard Template based on Bootstrap 5">
+    <meta name="author" content="NobleUI">
+    <meta name="keywords"
+        content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+
+    <title>NobleUI - HTML Bootstrap 5 Admin Dashboard Template</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- End fonts -->
+
+    <!-- core:css -->
+    <link rel="stylesheet" href="{{ asset('backendAdmin/assets/vendors/core/core.css') }}">
+    <!-- endinject -->
+
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="{{ asset('backendAdmin/assets/vendors/flatpickr/flatpickr.min.css') }}">
+    <!-- End plugin css for this page -->
+
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{ asset('backendAdmin/assets/fonts/feather-font/css/iconfont.css') }}">
+    <link rel="stylesheet" href="{{ asset('backendAdmin/assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+    <!-- endinject -->
+
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{ asset('backendAdmin/assets/css/demo2/style.css') }}">
+    <!-- End layout styles -->
+
+    <link rel="shortcut icon" href="{{ asset('backendAdmin/assets/images/favicon.png') }}" />
 </head>
+
 <body>
-    <h1> Men Admin Dashboard</h1>
+    <div class="main-wrapper">
+
+        <!-- partial:partials/_sidebar.html -->
+        @include('role.admin.layout.sidebar')
+
+        <!-- partial -->
+
+        <div class="page-wrapper">
+
+            <!-- partial:partials/_navbar.html -->
+            @include('role.admin.layout.header')
+            <!-- partial -->
+
+            <div class="page-content">
+                @yield('admin')
+            </div>
+
+            <!-- partial:partials/_footer.html -->
+            @include('role.admin.layout.footer')
+            <!-- partial -->
+
+        </div>
+    </div>
+
+    <!-- core:js -->
+    <script src="{{ asset('backendAdmin/assets/vendors/core/core.js') }}"></script>
+    <!-- endinject -->
+
+    <!-- Plugin js for this page -->
+    <script src="{{ asset('backendAdmin/assets/vendors/flatpickr/flatpickr.min.js') }}"></script>
+    <script src="{{ asset('backendAdmin/assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
+    <!-- End plugin js for this page -->
+
+    <!-- inject:js -->
+    <script src="{{ asset('backendAdmin/assets/vendors/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('backendAdmin/assets/js/template.js') }}"></script>
+    <!-- endinject -->
+
+    <!-- Custom js for this page -->
+    <script src="{{ asset('backendAdmin/assets/js/dashboard-dark.js') }}"></script>
+    <!-- End custom js for this page -->
+
 </body>
+
 </html>
